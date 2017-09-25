a71c10139564a797c8527b79290582437250228c
Mohammad Joneidi <joneidi.moh65@gmail.com>
2017-09-25 18:23:54 +0330

    first commit

--- Fingerprint/Shared/Fingerprint.cs
+++ Fingerprint/Shared/Fingerprint.cs
@@ -6,34 +6,11 @@
     using System.Threading.Tasks;
     using Zebble;
 
-    public partial class Fingerprint : CustomRenderedView<Renderer.FingerprintRenderer>
+    public partial class Fingerprint
     {
-	    // ---------------------- TODO: PROPERTIES -------------------------        
-		// Use the following pattern for each property.
-		MyPropertyType myProperty; // Used to hold the value.
-		internal readonly AsyncEvent MyPropertyChanged = new AsyncEvent(); // Used to cascade changes to the native object.
-		
-		public MyPropertyType MyProperty // The public api for your component
-		{
-		    get => myProperty;
-			set { if (myProperty != value) { myProperty = value; MyPropertyChanged.Raise(); } }
-		}
+        public async Task CreateFingerprint()
+        {
 
-
-        // ---------------------- TODO: METHODS -------------------------        
-        // For each method that you need, provide an internal delegate, and a public method.
-        // Then provide the delegate's implementation in each platform's Renderer class.
-        internal Action MyMethod1Implementation;
-        public void MyMethod1() => MyMethod1Implementation?.Invoke();
-
-        internal Func<int, string> MyMethod2Implementation;
-        public string MyMethod2(int param1) => MyMethod2Implementation?.Invoke(param1);
-
-
-
-        // ---------------------- TODO: EVENTS -------------------------        
-        // For each event that you need, provide an AsyncEvent object.
-        public readonly AsyncEvent MyEvent1 = new AsyncEvent();
-        public readonly AsyncEvent<TEventArg> MyEvent2 = new AsyncEvent<TEventArg>();
+        }
     }
 }
---
+++ Fingerprint/iOS/Fingerprint.cs
@@ -0,0 +1,14 @@
+namespace Zebble.Plugin.Renderer
+{
+    using System;
+    using System.ComponentModel;
+    using Zebble;
+    using System.Threading.Tasks;
+    using TheNativeType = System.Object; //TODO: replace with the native iOS type;
+
+
+    public partial class Fingerprint
+    {
+
+    }
+}
